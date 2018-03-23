92b4dcf76d0ef9d4cbefe186253fd036f04429f8
Lyubovbogdanova <37695053+Lyubovbogdanova@users.noreply.github.com>
2018-03-23 13:46:26 +0300

    Add files via upload

---
+++ formreg/userlist.sql
@@ -0,0 +1,72 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.9
+-- https://www.phpmyadmin.net/
+--
+-- Хост: 127.0.0.1
+-- Время создания: Мар 23 2018 г., 10:29
+-- Версия сервера: 10.1.31-MariaDB
+-- Версия PHP: 5.6.34
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `userlistdb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `userlist`
+--
+
+CREATE TABLE `userlist` (
+  `id` int(11) NOT NULL,
+  `full_name` varchar(32) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  `email` varchar(32) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  `username` varchar(20) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  `password` varchar(32) COLLATE utf8_unicode_ci NOT NULL DEFAULT ''
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Дамп данных таблицы `userlist`
+--
+
+INSERT INTO `userlist` (`id`, `full_name`, `email`, `username`, `password`) VALUES
+(1, 'asdf', 'asdf@asd', 'qwer', 'qwer'),
+(2, 'user', 'abc@cba', 'user', 'asdfgh'),
+(3, 'abc', 'abc@cba', 'abc', 'asdfg');
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `userlist`
+--
+ALTER TABLE `userlist`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `userlist`
+--
+ALTER TABLE `userlist`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
